2af453d3d02de3c543e638608d9bcd7aaff62aff
RidmieWee <ridmieweerakotuwa1999@gmail.com>
2023-05-09 13:33:03 +0100

    read function added

--- 21090263.py
+++ 21090263.py
@@ -13,56 +13,27 @@ import sklearn.cluster as cluster
 import sklearn.metrics as skmet
 import cluster_tools as ct
 
-# create function for read file
-def read_climate_data(filename):
-    """
-    This function reads climate change data file included in World
-    Bank climate data and returns two dataframes:
-    one with years as columns and one with countries as columns.
-    """
+def read_data_dile(filename):
 
     # read data from csv
     df_data = pd.read_csv(filename, skiprows=4)
 
-    # create a new dataframe to filter five usefull indicators
-    df_climate_change = df_data[
-        (df_data["Indicator Name"] == "Urban population") |
-        (df_data["Indicator Name"] == "Forest area (% of land area)") |
-        (df_data["Indicator Name"] == "CO2 emissions (metric tons per capita)")
-    ].reset_index(drop=True)
-
-    df_climate_change["Indicator Name"] = df_climate_change["Indicator Name"].replace(
-        ["Urban population",
-         "Forest area (% of land area)",
-         "CO2 emissions (metric tons per capita)"],
-        ["Urban population",
-         "Forest area(%)",
-         "CO2 emissions(mt)"])
-
     # drop all unnecessary columns
-    df_climate_change = df_climate_change.drop(["Indicator Code",
-                                                "Unnamed: 66",
-                                                "2020",
-                                                "2021"], axis=1)
+    df_data = df_data.drop(["Indicator Code", "Unnamed: 66"], axis=1)
 
     # drop the years between 1960 to 1990
-    df_climate_change = df_climate_change.drop(df_climate_change.iloc[:, 3:33],
-                                               axis=1)
+    df_data = df_data.drop(df_data.iloc[:, 3:33], axis=1)
 
     # create a dataframe to get years as columns
-    df_year = df_climate_change.copy()
-
-    # remove all NaNs to clean the dataframe
-    df_year = df_climate_change.dropna(axis=0)
+    df_year = df_data.copy()
 
     # set the country name as index
-    df_climate_change = df_climate_change.set_index("Country Name")
+    df_country = df_data.set_index("Country Name")
 
     # transpose the dataframe to get countries as columns
-    df_country = df_climate_change.transpose()
+    df_country = df_country.transpose()
 
     # clean the transposed dataframe
     df_country = df_country.dropna(axis=1)
 
-    # return both year and country dataframes
     return df_year, df_country
